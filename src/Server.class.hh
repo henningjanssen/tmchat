1b0dc616fe6dc8ce61e7729fa81e1abaac23cdff
Tim Siebels <tim_siebels_aurich@yahoo.de>
2015-08-19 22:19:26 +0200

    Fix typecheck error where variables would be created in socket_accept
    Summary: Yes, these are automatically generated. But hh_client wants to be explicit about it
    
    Test Plan: run hh_client
    
    Reviewers: henningj
    
    Reviewed By: henningj
    
    Differential Revision: http://phabricator.studentennachhilfe.net/D29

--- src/Server.class.hh
+++ src/Server.class.hh
@@ -1,15 +1,17 @@
 <?hh
-  class Server{
+  class Server {
     private $sock = null;
     public function __construct(
       string $host, int $port, string $pem, string $pempass
-    ) : void {
+    ): void {
       $context = stream_context_create();
       stream_context_set_option($context, "ssl", "local_cert", $pem);
       stream_context_set_option($context, "ssl", "passphrase", $pempass);
       stream_context_set_option($context, "ssl", "allow_self_signed", true);
       stream_context_set_option($context, "ssl", "verify_peer", false);
 
+      $errmsg = null;
+      $errno = null;
       $this->sock = stream_socket_server(
         "tcp://$host:$port",
         $errno,
@@ -21,11 +23,11 @@
       stream_socket_enable_crypto($this->sock, false);
       stream_set_blocking($this->sock, 0);
     }
-    public function accept() : ?int {//Changed to some class later. placeholder
+    public function accept(): mixed {//Changed to some class later. placeholder
       $new = socket_accept($this->sock);
       return $new;
     }
-    public function __destruct() : void {
+    public function __destruct(): void {
       fclose($this->sock);
     }
   }
